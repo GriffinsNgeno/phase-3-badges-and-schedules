e034c935fd697a9fac6ff8c0a3f3b72b2e02b786
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-02-10 12:06:34 +0300

    Completeed Lab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker name 
+    "Hello, my name is #{name}."
+end
+badge_maker("Griffins")
+
+def batch_badge_creator array
+    new_array=[]
+    array.each {|elem| new_array.push("Hello, my name is #{elem}.")}
+    new_array
+end
+
+def assign_rooms array
+    room=[]
+    array.each.with_index {|name,index| room.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+    room
+end
+
+def printer attendees
+    batch_badge_creator(attendees).map{|phrase| puts phrase}
+    assign_rooms(attendees).map{|room| puts room}
+end
